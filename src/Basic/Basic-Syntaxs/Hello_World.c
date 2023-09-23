cb156088a99015ffee2110839ab8f43f5e0edfa9
muxammad <swe.muxammadali@gmail.com>
2023-09-23 21:34:15 +0500

    Hello World in C

---
+++ src/Basic/Basic-Syntaxs/Hello_World.c
@@ -0,0 +1,14 @@
+// Preprocessor Directives
+#include <stdio.h>  // Include standard input/output library
+
+// Function Declarations
+int main(); // Declaration of the main function
+
+// Main Function
+int main() {
+    // Statements
+    printf("Hello, World!"); // Print a message to the console
+
+    // Return Statement
+    return 0; // Return an integer value to the operating system
+}
