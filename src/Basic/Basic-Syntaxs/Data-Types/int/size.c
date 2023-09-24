117b3150de3235fa05fbac6bc5b4f8d54235e37b
muxammad <swe.muxammadali@gmail.com>
2023-09-24 17:13:28 +0500

    sizes

--- src/Basic/Basic-Syntaxs/Data-Types/int/size.c
+++ src/Basic/Basic-Syntaxs/Data-Types/int/size.c
@@ -6,7 +6,6 @@ int main() {
     printf("short              | %d\n", sizeof(short));
     printf("int                | %d\n", sizeof(int));
     printf("long               | %d\n", sizeof(long));
-    printf("long double        | %d or %d (varies)\n", sizeof(long double), sizeof(long double) == 12 ? 10 : 12);
     printf("unsigned short     | %d\n", sizeof(unsigned short));
     printf("unsigned int       | %d\n", sizeof(unsigned int));
     printf("unsigned long      | %d\n", sizeof(unsigned long));
