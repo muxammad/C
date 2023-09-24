a2fd0786b88c0771be5469521740419c9bd86853
muxammad <swe.muxammadali@gmail.com>
2023-09-24 16:17:35 +0500

    int

--- src/Basic/Basic-Syntaxs/Data-Types/int/int.c
+++ src/Basic/Basic-Syntaxs/Data-Types/int/int.c
@@ -4,16 +4,12 @@ int main(){
 
     //declaring int data type with name "int_type"
     int int_type; // Size 4 bytes
-    
     //Assign a value to "int_type"
     int_type = 10;
-
     printf("Assigned int value: %d\n", int_type);
-
     //example
 
     printf("Enter an integer:  ");
-
     int number;
     //reads and stores 
     scanf("%d", &number);
