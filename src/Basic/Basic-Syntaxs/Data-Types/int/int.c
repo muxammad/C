853c16b14aceddaae3ecadff1acd1a90ed28c7d3
muxammad <swe.muxammadali@gmail.com>
2023-09-24 14:32:30 +0500

    int.c

---
+++ src/Basic/Basic-Syntaxs/Data-Types/int/int.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    //declaring int data type with name "int_type"
+    int int_type; // Size 4 bytes
+    
+    //Assign a value to "int_type"
+    int_type = 10;
+
+    printf("Assigned int value: %d ", int_type);
+    return 0;
+}
