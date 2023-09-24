070f90b6d6c5d0078d301d8974e05faeeb1a5c93
muxammad <swe.muxammadali@gmail.com>
2023-09-24 15:48:30 +0500

    int type example

--- src/Basic/Basic-Syntaxs/Data-Types/int/int.c
+++ src/Basic/Basic-Syntaxs/Data-Types/int/int.c
@@ -8,6 +8,17 @@ int main(){
     //Assign a value to "int_type"
     int_type = 10;
 
-    printf("Assigned int value: %d ", int_type);
+    printf("Assigned int value: %d\n", int_type);
+
+    //example
+
+    printf("Enter an integer:  ");
+
+    int number;
+    //reads and stores 
+    scanf("%d", &number);
+    // display number
+    printf("You Entered: %d\n\n", number);
+
     return 0;
 }
