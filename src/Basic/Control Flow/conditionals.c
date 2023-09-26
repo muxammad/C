f8cda543dbb350b1046d55adea98a816edb0c2b4
muxammad <swe.muxammadali@gmail.com>
2023-09-26 19:49:50 +0500

    conditinals

--- src/Basic/Control Flow/conditionals.c
+++ src/Basic/Control Flow/conditionals.c
@@ -10,13 +10,34 @@ int main(){
     // checks entered integer for ever or nit
     if (number % 2 == 0)
     {
-        printf("%d integer is even: ", number);
+        printf("%d integer is even: \n", number);
     }
     // else entered number is odd
     else{
-        printf("%i is odd integer ",number);
+        printf("%i is odd integer\n",number);
     }
     
+    int num1,num2;
 
+    printf("Enter two numbers:");
+    scanf("%d %d", &num1, &num2);
+
+    //checks if the two integers are equal.
+    if (num1 == num2)
+    {
+        printf("%d = %d", num1, num2);
+    }
+        
+    //checks if number1 is greater than number2.
+    else if (num1 < num2)
+    {
+        printf("%d < %d", num1, num2);
+    }
+    
+    //checks if both test expressions are false
+    else{
+        printf("%d > %d\n", num2,num1);
+    }
+    
     return 0;
 }
